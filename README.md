91d9ea5654a845b61ec3e9e985d3c9f509fcad94
Akira Matsushita <op.starlight@gmail.com>
2017-05-16 18:12:47 +0900

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# moodle2md
+# Moodle2md
+
+Moodleに提出されたオンラインテキストをマークダウンファイルに変換します
+
+
+## ディレクトリ説明
+- submit_files : 提出されたオンラインテキストを一括DLしたものを格納するディレクトリ
+
+
+## 実行方法
+
+1. `$ bundle install --path vendor/bundle`
+2. `$ bundle exec ruby run.rb`
---
+++ run.rb
@@ -0,0 +1,25 @@
+require 'rubygems'
+require 'bundler/setup'
+require 'nokogiri'
+require 'reverse_markdown'
+
+arr =  Dir.glob("submit_files/**/*.html")
+
+c = ""
+arr.each do |i|
+  f = File.open(i)
+  html = f.read
+  f.close
+  page = Nokogiri::HTML.parse(html, nil, 'UTF-8')
+  html_text = page.search('body').children.to_s
+  
+  md_text = ReverseMarkdown.convert html_text
+
+  md_text.gsub!(/&nbsp;/, '')
+  c += md_text + "---\n"
+
+end
+
+f = File.open("output.md", 'w')
+f.write(c)
+f.close
