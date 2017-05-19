4ce028f409301701f9c92036aacc9757c6e40bbf
Akira Matsushita <op.starlight@gmail.com>
2017-05-19 17:31:45 +0900

    enterd submitter name into md file

--- run.rb
+++ run.rb
@@ -3,17 +3,19 @@ require 'bundler/setup'
 require 'nokogiri'
 require 'reverse_markdown'
 
-arr =  Dir.glob("submit_files/**/onlinetext.html")
+files =  Dir.glob("submit_files/**/onlinetext.html")
 
 c = ""
-arr.each do |i|
-  f = File.open(i)
+files.each do |file|
+  f = File.open(file)
   html = f.read
   f.close
+  name = file.match(/\/.+\/([\p{Han}\s]+)_\d+_assignsubmission_onlinetext_\/onlinetext\.html\z/)[1]
   page = Nokogiri::HTML.parse(html, nil, 'UTF-8')
   html_text = page.search('body').children.to_s
   
-  md_text = ReverseMarkdown.convert html_text
+  md_text = '## ' + name + "\n"
+  md_text += ReverseMarkdown.convert html_text
 
   md_text.gsub!(/&nbsp;/, '')
   c += md_text + "\n---\n\n"
